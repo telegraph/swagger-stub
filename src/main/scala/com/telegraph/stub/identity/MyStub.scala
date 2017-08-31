8e8fc75a60592f275dd0f0a30e37d3d27d42a1a6
Parshotam Toora <toorap@TMGM-C02T62PLFVH4.local>
2017-08-31 16:33:19 +0100

    integrate validation

--- src/main/scala/com/telegraph/stub/identity/MyStub.scala
+++ src/main/scala/com/telegraph/stub/identity/MyStub.scala
@@ -14,28 +14,36 @@ object MyStub extends BaseStub {
 
   // map of action -> map  of pre-state, post-state
   override var stateTransitions =    Map(
-    "get" -> Map(Scenario.STARTED -> Scenario.STARTED))
+    "post" -> Map(Scenario.STARTED -> Scenario.STARTED))
 
 
   override def setUpMocks(cannedResponsesPath: String): Unit  = {
 
     // happy path get
-    stateTransitions("get") foreach {
+    stateTransitions("post") foreach {
 
       case (inState, outState) =>
-        wireMockServer.stubFor(get(urlMatching(".*/it"))
+        wireMockServer.stubFor(post(urlMatching(".*/tokens"))
+          .withRequestBody(equalToJson("{\"grant_type\":\"password\"}",true,true))
           .inScenario("state")
           .whenScenarioStateIs(inState)
           .withPostServeAction("validate-contract",new ContractValidationParameter("validate-contract"))
           .willReturn(
             aResponse()
               .withHeader("Content-Type", "application/json")
-              .withBody("{ \"first\":\"Hello\", \"second\":\"World\" }")
-              .withStatus(200))
+              .withBody(
+                "{"+
+                  "\"access_token\": \"MUpTUzcvMHVGUDQ1dDVsQnNQcWNNNEhHdWhzUm1OamdaZmExVmM5RzArak16VGRtR0xrM29BRjZKY1dCRGxibEg2Y01VaVJLNDZBPQ\","+
+                  "\"token_type\": \"Bearer\","+
+                  "\"refresh_token\": \"MUpTUzcvMHVGUDQ1dDVsQnNQcWNNNEhHdWhzUm1OamdaZmExVmM5RzArak16VGRtR0xrM29BRjZKY1dCRGxibEg2Y01VaVJLNDZBPQ\","+
+                  "\"expires_in\": 3600,"+
+                  "\"id_token\": \"eyJhbGciOiJub25lIn0.ewoJInByb2ZpbGUiOiB7CgkJInBJZCI6ICJiODFlYjM2Mi05NWQ4LTQ4YzAtOTY4MS1hOGJiZTg1Y2UxNjYiLAoJCSJjb21tZW50VG9rZW4iOiAiZXlKMWMyVnlibUZ0WlNJNkltMW5ZWEpuTmlJc0ltbGtJam9pTWpNME1qTTBNekkwTFRGbE1USXlNV1V4TWkweE1tVXhNbVV4TW1VdE1qRmxNVEpsTVRKbElpd2lZWFpoZEdGeUlqb2laSGRqZDJOM1pTSXNJblZ5YkNJNkltaDBkSEE2THk5dGVTNTBaV3hsWjNKaGNHZ3VZMjh1ZFdzdmJXVnRZbVZ5Y3k5dFoyRnlaellpZlE9PSBhYzEzNjRkOTIwNjE5NWRlZGYxNmYzODIxNjE1MjA2NDc3MWQ5ZWE5IDEzODA3MDYzODIiLAoJCSJmaXJzdE5hbWUiOiAiU2ltb24iLAoJCSJsYXN0TmFtZSI6ICJCYXluZXMiLAoJCSJ1c2VybmFtZSI6ICJleGFtcGxlIiwKCQkiZW1haWwiOiAiZXhhbXBsZUBnbWFpbC5jb20iLAoJCSJyZWFkZXJUeXBlIjogInN1YnNjcmliZXIiLCAvLyBpdCBjb3VsZCBiZSBhbm9ueW1vdXMsIHJlZ2lzdGVyZWQgb3Igc3Vic2NyaWJlcgoJCSJwZXJtaXNzaW9ucyI6IHsKCQkJImFjY2Vzc1dlYiI6IHRydWUsCgkJCSJhY2Nlc3NTbWFydHBob25lQXBwcyI6IHRydWUsCgkJCSJhY2Nlc3NUYWJsZXRBcHBzIjogdHJ1ZSwKCQkJImNhblBvc3RDb21tZW50cyI6IGZhbHNlLAoJCQkiYWNjZXNzTG95YWx0eSI6IHRydWUsCgkJCSJjYW5TdWJzY3JpYmUiOiBmYWxzZQoJCX0KCX0sCgkic2Vzc2lvbiI6IHsKCQkiaWQiOiAiMTYzOTViOTAtMWJjOC00ZmQ2LThhNjYtNzBhNmNhMDUxZjk1IiwKCQkic2VjdXJlSWQiOiAiNmE0MzljNWItNzljNi00NGU5LTlhYmQtZDJmN2IwYWUzMmQyIgoJfSwKCSJhbmFseXRpY3MiOiB7CgkJCSJsb2NhdGlvbiI6ICIiLAoJCQkidHJpYWxEZXNjcmlwdGlvbiI6ICIiLAoJCQkic3Vic2NyaXB0aW9uVHlwZSI6ICIiLAoJCQkiZmxvd1R5cGUiOiAiIiwKCQkJInByb2R1Y3RUeXBlIjogIiIsCgkJCSJ0b2tlblNvdXJjZSI6ICIiLAoJCQkidG9rZW5WYWx1ZSI6ICIiLAoJCQkiZGlzY291bnRWb3VjaGVyIjogIiIsCgkJCSJjdXJyZW50Q29tcGFpZ24iOiAib3BlbiBvZmZlciIsCgkJCSJzb3VyY2VDb21wYWlnbiI6ICJvcGVuIG9mZmVyIiwKCQkJInByb21vVHlwZSI6ICIiLAoJCQkidENvZGUiOiAiIiwKCQkJInN0YXR1cyI6IGZhbHNlLAoJCQkiYWNxdWlzaXRpb25Db21wbGV0ZWQiOiBmYWxzZSwKCQkJInByaWNlIjogIiIsCgkJCSJwcmljZURlc2NyaXB0aW9uIjogIiIsCgkJCSJzZXJ2aWNlSWQiOiAiIiwKCQkJInNlcnZpY2VQcmljZUlkIjogIiIsCgkJCSJkeW5hbWljUHJpY2UiOiAiQkxBTksiLAoJCQkicGF5bWVudFJlcXVpcmVkIjogZmFsc2UKCX0KfQ.\""+
+                "}"
+                )
+              .withStatus(201))
           .willSetStateTo(outState)
         )
     }
-
   }
 
   // driver class
