f3a2fb19a45d337844373013d5638331c8650a44
Parshotam Toora <toorap@TMGM-C02T62PLFVH4.local>
2017-08-31 13:26:47 +0100

    add validation to post call

--- src/main/scala/com/telegraph/stub/identity/BaseStub.scala
+++ src/main/scala/com/telegraph/stub/identity/BaseStub.scala
@@ -3,6 +3,10 @@ package com.telegraph.stub.identity
 import com.github.tomakehurst.wiremock.WireMockServer
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration.options
 import com.atlassian.oai.validator.wiremock.SwaggerValidationListener
+import com.atlassian.oai.validator.wiremock.SwaggerValidationListener.SwaggerValidationException
+import com.github.tomakehurst.wiremock.core.Admin
+import com.github.tomakehurst.wiremock.extension.PostServeAction
+import com.github.tomakehurst.wiremock.stubbing.ServeEvent
 
 import scala.io.Source
 
@@ -26,7 +30,7 @@ abstract class BaseStub {
     if (inputSwaggerFile != null)
       swaggerFile = inputSwaggerFile
 
-    wireMockServer = new WireMockServer(options().port(port))
+    wireMockServer = new WireMockServer(options().port(port).extensions(new ValidateContractAction))
     wireMockListener = new SwaggerValidationListener(Source.fromFile(swaggerFile).mkString)
     wireMockServer.addMockServiceRequestListener(wireMockListener)
     setUpMocks(cannedResponsesPath)
@@ -36,26 +40,51 @@ abstract class BaseStub {
 
   // start server
   def start = {
-    wireMockServer.start()
+    if (wireMockServer!=null && !wireMockServer.isRunning)
+      wireMockServer.start()
   }
 
-  // normally this would be the step that catches the contract validation errors
-  // but for this example we are checking in the tests explicitly
-  // there are alternative approaches
+  // stop server
   def stop = {
-    //validateContract
+    if (wireMockServer==null)
+      throw new Exception("Wiremock server may have found an invalid contract - please check logs")
     wireMockServer.stop
   }
 
-  // validate contract
+  // validate contract and if invalid output error and bail out
   def validateContract = {
-    wireMockListener.assertValidationPassed()
-    wireMockListener.reset()
+    try {
+      wireMockListener.assertValidationPassed()
+    } catch  {
+
+      case ex: Exception => {
+        println(ex.getLocalizedMessage)
+        ex.printStackTrace()
+        wireMockServer.resetAll()
+        wireMockServer.shutdown()
+        wireMockServer = null
+        throw ex
+      }
+    }
   }
 
   // action -> pre-start, post-state
   protected var stateTransitions : Map[String, Map[String,String]]
 
   protected def setUpMocks(cannedResponsesPath: String): Unit
+
+  // post serve action to validate contract
+  class ValidateContractAction extends PostServeAction {
+    override def doGlobalAction(serveEvent: ServeEvent, admin: Admin): Unit = {
+      validateContract
+    }
+    override def getName: String = "validate-contract"
+  }
+
+  // Simple Bean to get things done
+  class ContractValidationParameter(inputValidateName: String) extends Serializable {
+    var validateName: String = inputValidateName
+  }
+
 }
 
--- src/main/scala/com/telegraph/stub/identity/MyStub.scala
+++ src/main/scala/com/telegraph/stub/identity/MyStub.scala
@@ -14,7 +14,7 @@ object MyStub extends BaseStub {
 
   // map of action -> map  of pre-state, post-state
   override var stateTransitions =    Map(
-    "get" -> Map(Scenario.STARTED -> Scenario.STARTED, "exists" -> "exists"))
+    "get" -> Map(Scenario.STARTED -> Scenario.STARTED))
 
 
   override def setUpMocks(cannedResponsesPath: String): Unit  = {
@@ -26,6 +26,7 @@ object MyStub extends BaseStub {
         wireMockServer.stubFor(get(urlMatching(".*/it"))
           .inScenario("state")
           .whenScenarioStateIs(inState)
+          .withPostServeAction("validate-contract",new ContractValidationParameter("validate-contract"))
           .willReturn(
             aResponse()
               .withHeader("Content-Type", "application/json")
@@ -37,11 +38,11 @@ object MyStub extends BaseStub {
 
   }
 
+  // driver class
   def main(args : Array[String]) {
 
     // passed swagger file, port, canned responses file path
     MyStub.configureStub(args(0), args(1).toInt, args(2))
     MyStub.start
-
   }
 }
