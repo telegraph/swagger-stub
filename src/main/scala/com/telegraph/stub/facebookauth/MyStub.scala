5364dae8c19c57ca6ecbf53d29b6e7fc575e8354
Parshotam Toora <toorap@TMGM-C02T62PLFVH4.local>
2017-09-14 17:49:55 +0100

    add fbauth scenarios

--- src/main/scala/com/telegraph/stub/facebookauth/MyStub.scala
+++ src/main/scala/com/telegraph/stub/facebookauth/MyStub.scala
@@ -17,7 +17,7 @@ object MyStub extends SmartStub {
   override def setUpMocks(cannedResponsesPath: String): Unit  = {
 
     // happy path
-    wireMockServer.stubFor(options(urlMatching(".*/tmgauth"))
+    wireMockServer.stubFor(options(urlMatching("(.*)(tmgauth|fbauth)"))
       .willReturn(
         aResponse()
           .withTransformerParameter("nextState", "any")
@@ -27,7 +27,7 @@ object MyStub extends SmartStub {
           .withHeader("access-control-max-age", "86400")
           .withStatus(200)))
 
-    // happy path subscribed
+    // tmg subscribed
     wireMockServer.stubFor(post(urlMatching(".*/tmgauth"))
       .withRequestBody(equalToJson("{\"emailId\":\"subscriber@telegraph.co.uk\"}",true,true))
       .willReturn(
@@ -35,10 +35,11 @@ object MyStub extends SmartStub {
           .withTransformerParameter("nextState", "any")
           .withHeader("Content-Type", "application/json")
           .withHeader("access-control-allow-origin", "*")
-          .withBody(Source.fromFile(cannedResponsesPath+ "/tmgAuthSubscribed.json").mkString)
+          .withBody(Source.fromFile(cannedResponsesPath+ "/tmgAuthSubscribed.json").mkString
+            .replace("&apos;"," ").replace("&quot;","'"))
           .withStatus(200)))
 
-    // happy path registered
+    // tmg registered
     wireMockServer.stubFor(post(urlMatching(".*/tmgauth"))
       .withRequestBody(equalToJson("{\"emailId\":\"registered@telegraph.co.uk\"}",true,true))
       .willReturn(
@@ -49,7 +50,7 @@ object MyStub extends SmartStub {
           .withBody(Source.fromFile(cannedResponsesPath+ "/tmgAuthSubscribed.json").mkString)
           .withStatus(200)))
 
-    // unauthorised
+    // tmg unauthorised
     wireMockServer.stubFor(post(urlMatching(".*/tmgauth"))
       .withRequestBody(equalToJson("{\"emailId\":\"unauthorised@telegraph.co.uk\"}",true,true))
       .willReturn(
@@ -60,7 +61,7 @@ object MyStub extends SmartStub {
           .withBody(Source.fromFile(cannedResponsesPath+ "/tmgAuth401.json").mkString)
           .withStatus(401)))
 
-    // system error
+    // tmg system error
     wireMockServer.stubFor(post(urlMatching(".*/tmgauth"))
       .withRequestBody(equalToJson("{\"emailId\":\"systemError@telegraph.co.uk\"}",true,true))
       .willReturn(
@@ -71,7 +72,7 @@ object MyStub extends SmartStub {
           .withBody(Source.fromFile(cannedResponsesPath+ "/tmgAuth500.json").mkString)
           .withStatus(500)))
 
-    // unavailable
+    // tmg unavailable
     wireMockServer.stubFor(post(urlMatching(".*/tmgauth"))
       .withRequestBody(equalToJson("{\"emailId\":\"unavailable@telegraph.co.uk\"}",true,true))
       .willReturn(
@@ -82,15 +83,60 @@ object MyStub extends SmartStub {
           .withBody(Source.fromFile(cannedResponsesPath+ "/tmgAuth503.json").mkString)
           .withStatus(503)))
 
-    // happy path fb
+    // fb subscribed
     wireMockServer.stubFor(post(urlMatching(".*/fbauth"))
+      .withRequestBody(equalToJson("{\"accountLinkingToken\":\"subscr123456\"}",true,true))
       .willReturn(
         aResponse()
           .withTransformerParameter("nextState", "any")
           .withHeader("Content-Type", "application/json")
           .withHeader("access-control-allow-origin", "*")
-          .withBody(Source.fromFile(cannedResponsesPath+ "/fbAuthSubscribed.json").mkString)
+          .withBody(Source.fromFile(cannedResponsesPath+ "/tmgAuthSubscribed.json").mkString)
           .withStatus(200)))
+
+    // fb registered
+    wireMockServer.stubFor(post(urlMatching(".*/fbauth"))
+      .withRequestBody(equalToJson("{\"accountLinkingToken\":\"regist123456\"}",true,true))
+      .willReturn(
+        aResponse()
+          .withTransformerParameter("nextState", "any")
+          .withHeader("Content-Type", "application/json")
+          .withHeader("access-control-allow-origin", "*")
+          .withBody(Source.fromFile(cannedResponsesPath+ "/tmgAuthSubscribed.json").mkString)
+          .withStatus(200)))
+
+    // fb unauthorised
+    wireMockServer.stubFor(post(urlMatching(".*/fbauth"))
+      .withRequestBody(equalToJson("{\"accountLinkingToken\":\"unauth123456\"}",true,true))
+      .willReturn(
+        aResponse()
+          .withTransformerParameter("nextState", "any")
+          .withHeader("Content-Type", "application/json")
+          .withHeader("access-control-allow-origin", "*")
+          .withBody(Source.fromFile(cannedResponsesPath+ "/tmgAuth401.json").mkString)
+          .withStatus(401)))
+
+    // fb system error
+    wireMockServer.stubFor(post(urlMatching(".*/fbauth"))
+      .withRequestBody(equalToJson("{\"accountLinkingToken\":\"errors123456\"}",true,true))
+      .willReturn(
+        aResponse()
+          .withTransformerParameter("nextState", "any")
+          .withHeader("Content-Type", "application/json")
+          .withHeader("access-control-allow-origin", "*")
+          .withBody(Source.fromFile(cannedResponsesPath+ "/tmgAuth500.json").mkString)
+          .withStatus(500)))
+
+    // fb unavailable
+    wireMockServer.stubFor(post(urlMatching(".*/fbauth"))
+      .withRequestBody(equalToJson("{\"accountLinkingToken\":\"unavai123456\"}",true,true))
+      .willReturn(
+        aResponse()
+          .withTransformerParameter("nextState", "any")
+          .withHeader("Content-Type", "application/json")
+          .withHeader("access-control-allow-origin", "*")
+          .withBody(Source.fromFile(cannedResponsesPath+ "/tmgAuth503.json").mkString)
+          .withStatus(503)))
 
   }
 
