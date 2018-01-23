c5b36a919ad6f6b75e8ab45092ed3667e4e04501
Nancy Gupta <Nancy.Gupta@telegraph.co.uk>
2018-01-23 09:41:39 +0000

    Pushing cruise listing changes

--- build.sbt
+++ build.sbt
@@ -7,7 +7,7 @@ lazy val root = (project in file(".")).
     inThisBuild(List(
       organization := "com.telegraph.stub.cruisestub",
       scalaVersion := "2.11.8",
-      version      := "0.0.1"
+      version      := "0.0.3"
     )),
     name := "cruisestub",
     ServiceDependencies
--- src/main/scala/com/telegraph/stub/cruisestub/MyStub.scala
+++ src/main/scala/com/telegraph/stub/cruisestub/MyStub.scala
@@ -33,6 +33,87 @@ object MyStub extends SmartStub {
             .replace("&apos;"," ").replace("&quot;","'"))
           .withStatus(200)))
 
+    wireMockServer.stubFor(get(urlMatching(".*/pClvcx3M5N4"))
+      .willReturn(
+        aResponse()
+          .withTransformerParameter("nextState", "any")
+          .withHeader("Content-Type", "application/json")
+          .withBody(Source.fromFile(cannedResponsesPath+ "/britishAisle.json").mkString
+            .replace("&apos;"," ").replace("&quot;","'"))
+          .withStatus(200)))
+
+    wireMockServer.stubFor(get(urlMatching(".*/ng5w8LfZ8Wl"))
+      .willReturn(
+        aResponse()
+          .withTransformerParameter("nextState", "any")
+          .withHeader("Content-Type", "application/json")
+          .withBody(Source.fromFile(cannedResponsesPath+ "/britishDiscoveryAisle.json").mkString
+            .replace("&apos;"," ").replace("&quot;","'"))
+          .withStatus(200)))
+
+    wireMockServer.stubFor(get(urlMatching(".*/ng5xKjcstfs"))
+      .willReturn(
+        aResponse()
+          .withTransformerParameter("nextState", "any")
+          .withHeader("Content-Type", "application/json")
+          .withBody(Source.fromFile(cannedResponsesPath+ "/britishIslesDiscovery.json").mkString
+            .replace("&apos;"," ").replace("&quot;","'"))
+          .withStatus(200)))
+
+    wireMockServer.stubFor(get(urlMatching(".*/mlMWvMk8tGh"))
+      .willReturn(
+        aResponse()
+          .withTransformerParameter("nextState", "any")
+          .withHeader("Content-Type", "application/json")
+          .withBody(Source.fromFile(cannedResponsesPath+ "/easterSpring.json").mkString
+            .replace("&apos;"," ").replace("&quot;","'"))
+          .withStatus(200)))
+
+    wireMockServer.stubFor(get(urlMatching(".*/mlMWvMxfHXq"))
+      .willReturn(
+        aResponse()
+          .withTransformerParameter("nextState", "any")
+          .withHeader("Content-Type", "application/json")
+          .withBody(Source.fromFile(cannedResponsesPath+ "/grandScottishLochs.json").mkString
+            .replace("&apos;"," ").replace("&quot;","'"))
+          .withStatus(200)))
+
+    wireMockServer.stubFor(get(urlMatching(".*/mlMWvMdxpST"))
+      .willReturn(
+        aResponse()
+          .withTransformerParameter("nextState", "any")
+          .withHeader("Content-Type", "application/json")
+          .withBody(Source.fromFile(cannedResponsesPath+ "/icelandCruise.json").mkString
+            .replace("&apos;"," ").replace("&quot;","'"))
+          .withStatus(200)))
+
+    wireMockServer.stubFor(get(urlMatching(".*/ng5xFhmBC33"))
+      .willReturn(
+        aResponse()
+          .withTransformerParameter("nextState", "any")
+          .withHeader("Content-Type", "application/json")
+          .withBody(Source.fromFile(cannedResponsesPath+ "/londonNewYear.json").mkString
+            .replace("&apos;"," ").replace("&quot;","'"))
+          .withStatus(200)))
+
+    wireMockServer.stubFor(get(urlMatching(".*/mlMWvMxfHXq"))
+      .willReturn(
+        aResponse()
+          .withTransformerParameter("nextState", "any")
+          .withHeader("Content-Type", "application/json")
+          .withBody(Source.fromFile(cannedResponsesPath+ "/scottishHighlights.json").mkString
+            .replace("&apos;"," ").replace("&quot;","'"))
+          .withStatus(200)))
+
+    wireMockServer.stubFor(get(urlMatching(".*/ng5w7ZGDGzZ"))
+      .willReturn(
+        aResponse()
+          .withTransformerParameter("nextState", "any")
+          .withHeader("Content-Type", "application/json")
+          .withBody(Source.fromFile(cannedResponsesPath+ "/summerGrandBritish.json").mkString
+            .replace("&apos;"," ").replace("&quot;","'"))
+          .withStatus(200)))
+
   }
 
   // driver class
